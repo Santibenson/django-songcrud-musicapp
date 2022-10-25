92a453738bfe7ecd1dde9dc19256fca9c5f0cf85
Santibenson <114431556+Santibenson@users.noreply.github.com>
2022-10-25 01:15:06 +0100

    v2.
    I was able to add the models, register them, create a superuser, make migrations, and perform CRUD operations from the database.

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    age = models.IntegerField()
+    artiste_img_url = models.CharField(max_length=2083, default='', blank=True)
+
+    def __str__(self):
+        if self.first_name:
+            return f'{self.first_name} {self.last_name}'
+        return self.first_name
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=500)
+    date_released = models.DateTimeField('date released')
+    likes = models.IntegerField()
+    song_img_url = models.CharField(max_length=2083, default='', blank=True)
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+
+    def __str__(self):
+        if self.title:
+            return f'[Song title: {self.title}]  [likes: {self.likes}]'
+        return self.title
+
+
+class Lyric(models.Model):
+    content = models.CharField(max_length=5000)
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
+
+    def __str__(self):
+        if self.song_id:
+            return f'{self.song_id}'
+        return self.song_id
--- songcrud/musicapp/views.py
+++ songcrud/musicapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def music(request):
-    return HttpResponse("<h1>Hello, Zuri</h1>")
+    return render(request, 'index.html')
